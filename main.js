9532f370cbe2295314c55e5d399c804b76a2579e
GISELE RODRIGUES DOS SANTOS <25203682@dsn127m1247444.cfp127aluno.local>
2026-02-24 15:50:38 -0300

    validações feitas

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 "use strict"
 
 function adicionarProduto(){
-    const produto = document.getElementById('produto')
+   
+    const produto= document.getElementById('produto')
     const lista = document.getElementById('lista')
 
     const span = document.createElement('span') 
     span.textContent = produto.value
-    span.className = "px-8 py-2 bg-green-200"
-
+  
+    
+    if(produto.value == ""){
+        return false
+    }else{
+    span.className = "px-8 py-2"
     lista.appendChild(span)
+    limparCampo()
+    }
+
+}
+function limparCampo(){
+    document.getElementById('produto').value = ""
 }
