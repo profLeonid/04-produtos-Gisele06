c9d3c5467b03b77ba424f027d5d03cf678027f6d
GISELE RODRIGUES DOS SANTOS <25203682@dsn127m1247444.cfp127aluno.local>
2026-02-24 17:00:20 -0300

    novos campos de texto criados

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 "use strict"
 
+
+
 function adicionarProduto(){
-   
+    
     const produto= document.getElementById('produto')
     const lista = document.getElementById('lista')
 
-    const span = document.createElement('span') 
-    span.textContent = produto.value
-  
-    
-    if(produto.value == ""){
-        return false
-    }else{
+    const spanProduto = document.createElement('span') 
+    spanProduto.textContent = produto.value
     span.className = "px-8 py-2"
-    lista.appendChild(span)
-    limparCampo()
-    }
+
+    const codigo = document.getElementById('codigo')
+
+    const spanCodigo = document.createElement('span')
+    spanCodigo.textContent = codigo.value
+    span.className = "px-8 py-2"
+
+    const quantidade = document.getElementById('codigo')
+
+    const spanQuantidade = document.createElement('span')
+    spanQuantidade.textContent = quantidade.value
+    span.className = "px-8 py-2"
+
+    lista.appendChild(spanProduto)
+    lista.appendChild(spanCodigo)
+    lista.appendChild(spanQuantidade)
 
 }
-function limparCampo(){
-    document.getElementById('produto').value = ""
-}
+
+// function validarCampos(){
+//     if(produto.value == "" || codigo.value == ""|| quantidade.value == ""){
+//         console.log("arroz com batatinha")
+//     }
+// }
